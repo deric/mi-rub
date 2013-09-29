a4c4e58639a8dab9955fa8c04c72c2db5b818c6f
Tomas Barton <barton.tomas@gmail.com>
2013-09-29 23:10:20 +0200

    fixed description

--- 02/02_same_word.rb
+++ 02/02_same_word.rb
@@ -1,9 +1,9 @@
 #!/usr/bin/env ruby
 
-# Count frequencies of words in a given
+# Find duplication of words in a given
 # file and return a Hash indexed by the number
-# of occurances and containing an Array of 
-# words having that frequency in the given file
+# of lines and containing an Array of 
+# words that were repeated
 def same_word(file)
   # @TODO write implementation
 end
