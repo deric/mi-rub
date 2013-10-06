a7d3b80615538ddc8b5cffa5b40ada3139681598
Tomas Barton <barton.tomas@gmail.com>
2013-10-06 23:54:25 +0200

    added 1st homework

---
+++ hw1/my_array.rb
@@ -0,0 +1,8 @@
+#!/usr/bin/env ruby
+
+# Immitates some behaviour of the Array
+# clas
+class MyArray
+
+
+end
---
+++ hw1/spec/my_array_spec.rb
@@ -0,0 +1,170 @@
+require 'rspec'
+require 'spec_helper'
+require_relative '../my_array'
+
+describe MyArray do
+
+  it "should have a method for determining size" do
+    ary = MyArray.new
+    ary.size.should eq 0
+  end
+
+  it "should copy data from an Array" do
+    ary = MyArray.new([1, 2, 3])
+    ary.size.should eq 3
+  end
+
+  context "each" do
+    subject { MyArray.new(data) }
+
+    let(:data) { [2, 5, 3]}
+    its([0]) { should == 2 }
+    its([1]) { should == 5 }
+    its([2]) { should == 3 }
+
+    it "iterate over data" do
+      d = []
+      subject.each { |i|
+        d << i
+      }
+      d.should eq data
+    end
+
+    it "should work without passed block" do
+      subject.each
+    end
+
+    it "should return Enumerator" do
+      res = subject.each
+      res.class.should eq Enumerator
+      res.to_a.should eq data
+    end
+
+  end
+
+  context "reverse" do
+    subject { MyArray.new(data) }
+
+    let(:data) { [2, 5, 3]}
+    let(:revdata) { [3, 5, 2]}
+
+    it "should support reverse_each" do
+      d = []
+      MyArray.new([2, 5, 3]).reverse_each { |i|
+        d << i
+      }
+      d.should eq [3, 5, 2]
+    end
+
+    it "should work without passed block" do
+      subject.reverse
+    end
+
+    it "should return Array" do
+      res = subject.reverse
+      res.class.should eq Array
+      res.to_a.should eq revdata
+    end
+
+    it "should support self-modifying reverse!" do
+      subject.reverse!
+      d = []
+      subject.each {|i|
+        d << i
+      }
+      d.should eq [3, 5, 2]
+    end
+  end
+
+  context "pop" do
+    subject { MyArray.new(data) }
+
+    let(:data) { [2, 5, 3]}
+
+    it "should have method pop" do
+      subject.pop
+    end
+
+    it "should remove last element" do
+      elem = subject.pop
+      elem.should eq 3
+    end
+  end
+
+  context "select" do 
+    subject { MyArray.new(data) }
+
+    let(:data) { [1, 2, 3, 4, 5, 6]}
+
+    it "should select even numbers" do 
+      res = subject.select { |num|  num.even?  } 
+      res.should eq [2, 4, 6]
+    end 
+  end
+
+  context "collect" do
+    subject { MyArray.new(data) }
+
+    let(:data) { [ "a", "b", "c", "d" ]}
+
+    it "should iterate over elements and collect block results" do
+      res = subject.collect {|x| x << "!" }
+      res.should eq ["a!", "b!", "c!", "d!"]
+    end
+
+    it "should return Enumerator" do
+      res = subject.collect
+      res.class.should eq Enumerator
+    end
+  end
+
+  context "clear" do
+
+    subject { MyArray.new(data) }
+
+    let(:data) { [ "a", "b", "c", "d" ]}
+
+    it "should remove all elements" do
+      subject.clear
+      subject.size.should == 0
+    end
+  end
+
+
+  context "include?" do
+
+    subject { MyArray.new(data) }
+
+    let(:data) { [ 2, 5, 15, 23 ]}
+
+    it "should check whether element is in array" do
+      res = subject.include? 5
+      res.should == true
+    end
+  end
+
+
+  context "max" do
+
+    subject { MyArray.new(data) }
+
+    let(:data) { (1..50).to_a }
+
+    it "should return maximum value" do
+      res = subject.max
+      res.should == 50
+    end
+  end
+
+  context "max strings" do
+
+    subject { MyArray.new(data) }
+
+    let(:data) { %w(albatross dog horse) }
+
+    it "should compare length of strings" do
+      res = subject.max { |a, b| a.length <=> b.length }
+      res.should eq "albatross"
+    end
+  end
+end
