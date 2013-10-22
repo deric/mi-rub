2641708094281e017488f5c164418ca3fc42df4e
Tomas Barton <barton.tomas@gmail.com>
2013-10-22 12:05:29 +0200

    checking correct solution

--- hw2/spec/sudoku_spec.rb
+++ hw2/spec/sudoku_spec.rb
@@ -5,7 +5,9 @@ require 'sudoku'
 
 describe Sudoku do
   subject { Sudoku.new(game) }
-  let(:game) {'006002800050130020402500901900070540020801090087050002208009305040018070003600100'}
+  let(:game) do
+    '006002800050130020402500901900070540020801090087050002208009305040018070003600100'
+  end
 
   it 'should know when game is solved' do
     expect(subject.solved?).to eq false
@@ -15,23 +17,38 @@ describe Sudoku do
     expect { subject.solve }.to change {
       subject.solved?
     }.from(false).to(true)
+    expect(subject.solution).to eq(
+      '136492857859137426472586931961273548524861793387954612218749365645318279793625184'
+    )
   end
 
   describe 'simple' do
-    let(:game) {'010780000090600045002500100000000567800020003136000000007009400620007050000051030'}
+    let(:game) do
+      '010780000090600045002500100000000567800020003136000000007009400620007050000051030'
+    end
+
     it 'should solve' do
       expect { subject.solve }.to change {
         subject.solved?
       }.from(false).to(true)
+      expect(subject.solution).to eq(
+        '513784692798612345462593178249138567875426913136975284357869421621347859984251736'
+      )
     end
   end
 
   describe 'simple 2' do
-    let(:game) {'003020600900305001001806400008102900700000008006708200002609500800203009005010300'}
+    let(:game) do
+      '003020600900305001001806400008102900700000008006708200002609500800203009005010300'
+    end
+
     it 'should solve' do
       expect { subject.solve }.to change {
         subject.solved?
       }.from(false).to(true)
+      expect(subject.solution).to eq(
+        '483921657967345821251876493548132976729564138136798245372689514814253769695417382'
+      )
     end
   end
 
