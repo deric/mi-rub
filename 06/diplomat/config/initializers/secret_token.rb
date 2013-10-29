404e7e6bf1ca284320c07bfffa1d39e7207308f7
Tomas Barton <barton.tomas@gmail.com>
2013-10-29 14:19:15 +0100

    added rails diplomat project

---
+++ 06/diplomat/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Diplomat::Application.config.secret_key_base = 'ff042075d6eeb251faa30ee52134ab806c64c841310a4925a2755e38454e91666d2b695302c7f0ac214348bea0b8dc2d2d319bf72062a3dd7ca5ca7666f9754a'
---
+++ 06/diplomat/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Diplomat::Application.config.session_store :cookie_store, key: '_diplomat_session'
